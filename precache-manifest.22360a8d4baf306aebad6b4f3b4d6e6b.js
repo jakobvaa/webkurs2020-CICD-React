7fced38b7f29fa0a9e9554dd275a67021dccf894
Jakob Myklebust Vaardal <jakobmva@stud.ntnu.no>
2020-11-02 18:01:02 +0100

    Updates

---
+++ precache-manifest.22360a8d4baf306aebad6b4f3b4d6e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb96da76273c8344da76711f6d65fb83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "97e67d795fa475f2ed8e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e67d795fa475f2ed8e",
+    "url": "/static/js/main.df862932.chunk.js"
+  },
+  {
+    "revision": "f233014d846008652b8c",
+    "url": "/static/js/runtime-main.5ff388ac.js"
+  }
+]);
