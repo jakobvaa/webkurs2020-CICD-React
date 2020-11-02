7ba7ddba0aaea45e1c3bd357730cb8d565ae08dd
Jakob Myklebust Vaardal <jakobmva@stud.ntnu.no>
2020-11-02 18:51:24 +0100

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const styles = {
 const Header = () => {
     return (
         <div style={styles}>
-            <header>KART? Norkart.</header>
+            <header>KART?  Norkart.</header>
         </div>
     )
 }
